e772f64d10f24ca71b49865e221bb6483941689c
Daniel Lee <danlee@hey.com>
2022-09-15 21:29:31 -0600

    delete subscription broadcast

--- server/app/channels/chat_channel.rb
+++ server/app/channels/chat_channel.rb
@@ -1,7 +1,6 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
     stream_from "stream_test"
-    ActionCable.server.broadcast "stream_test", message: "You are now connected !", status: 200
   end
 
   def unsubscribed
